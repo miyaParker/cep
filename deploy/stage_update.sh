58184583465adcce1adf051ec3a54ade91762b4f
Co-creation Hub <mxlab@cchubnigeria.com>
2023-05-05 11:09:43 +0000

    feature/relearn-frontend/ci-cd-initial-commit

---
+++ deploy/stage_update.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# any future command that fails will exit the script
+set -e
+################## Backend update ###############################
+# Change directory to where the project is stored
+# cd /var/www/html/itunu-dashboard/
+
+cd /var/www/html/itunu-dashboard/
+
+# sudo git stash - incase of any local changes
+sudo git stash
+
+# sudo git fetch
+sudo git fetch
+
+# checkout develop
+sudo git checkout deve
+
+# # pull the changes
+sudo git pull origin dev
+
+
+# # install
+# sudo yarn install
+
+# # Build the application
+# sudo yarn build
+
+#echo "****************************************************************"
+#echo "--- Frontend update complete"
+#echo "****************************************************************"
