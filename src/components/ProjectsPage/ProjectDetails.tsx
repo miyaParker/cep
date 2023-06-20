3bd0d7e13ddee9778d62b3de56218eb8af1a7a27
Karen Ziboh <karen@cchubnigeria.com>
2023-06-20 09:27:30 +0100

    (Get Involved): adds key to lists

--- src/components/ProjectsPage/ProjectDetails.tsx
+++ src/components/ProjectsPage/ProjectDetails.tsx
@@ -11,7 +11,7 @@ const ProjectDetails = ({
 
     useEffect(() => {
         setDomReady(true)
-    })
+    }, [])
     const modalRoot = document.getElementById('modal-root')!
     return (<div>
         {
@@ -24,35 +24,39 @@ const ProjectDetails = ({
                             className="w-[90%] max-w-[640px] h-max max-h-[90vh] overflow-scroll relative rounded-[8px] px-[20px] md:pl-[44px]  bg-white">
                             <div onClick={closeModal}
                                  className='bg-[#F6F6F9] absolute top-[18px] right-[18px] lg:top-[24px] lg:right-[24px] cursor-pointer w-[28px] h-[28px] lg:w-[32px] lg:h-[32px] rounded-full flex justify-center items-center'>
-                                <Image src="/close.svg" alt={'close modal'} width={24} height={24} className={'w-[20px] lg:w-[20px]'}/>
+                                <Image src="/close.svg" alt={'close modal'} width={24} height={24}
+                                       className={'w-[20px] lg:w-[20px]'}/>
                             </div>
                             <p className="font-neue mt-[40px] font-bold text-[25px] leading-[120%] tracking-[0.125px] text-[#0E0E10]">{project.name}</p>
-                            {project.details.summary.map((summary: any) =>
-                                <p className="mt-[20px] max-w-[521px] font-matter text-[17px] leading-[140%] tracking-[0.17px] text-[#333438]">{summary}</p>
+                            {project.details.summary.map((summary: any, index: number) =>
+                                <p key={index}
+                                   className="mt-[20px] max-w-[521px] font-matter text-[17px] leading-[140%] tracking-[0.17px] text-[#333438]">{summary}</p>
                             )}
 
                             <p className="my-[20px] font-bold font-neue text-[20px] leading-[140%] tracking-[0.16px] text-[#0E0E10]">{project.details.info?.title}</p>
                             {project.details.info?.keyPoints?.map((keyPoint: string, index: number) =>
-                                <div className="flex items-start max-w-[521px]">
+                                <div key={index} className="flex items-start max-w-[521px]">
                                     <Image alt={`keypoint -${index}`} src={`/star-${index + 1}.svg`}
                                            className="mr-[12px]" width={20} height={20}/>
                                     <p className="mb-[20px] font-matter text-[17px] leading-[140%] tracking-[0.17px] text-[#333438]">{keyPoint}</p>
                                 </div>
                             )}
-                            {project.details.info?.description?.map((description: string) =>
-                                <p className="mt-[20px] max-w-[521px] font-matter text-[17px] leading-[140%] tracking-[0.17px] text-[#333438]">{description}</p>
+                            {project.details.info?.description?.map((description: string, index: number) =>
+                                <p key={index}
+                                   className="mt-[20px] max-w-[521px] font-matter text-[17px] leading-[140%] tracking-[0.17px] text-[#333438]">{description}</p>
                             )}
                             <p className="mb-[20px] mt-[8px] max-w-[521px] font-bold font-neue text-[20px] leading-[140%] tracking-[0.16px] text-[#0E0E10]">Impact</p>
-                            {project.impact.map((impact: any) => {
+                            {project.impact.map((impact: any, index: number) => {
                                 if (impact?.location) {
                                     return (
-                                        <div className="flex mr-[20px] mb-[20px] flex-wrap">
-                                            {impact?.info?.map((info: any) => <div
-                                                className="flex items-center mr-[20px] my-2 py-[17px] px-[12px] bg-[#F6F6F9] rounded-[4px]">
-                                                <p className="font-bold text-[28px] text-[#0E0E10] mr-[10px] font-neue">{info.number}</p>
-                                                <p
-                                                    className=" font-matter text-[17px] uppercase leading-[140%] tracking-[0.17px] text-[#333438]">{info.description}</p>
-                                            </div>)}
+                                        <div key={index} className="flex mr-[20px] mb-[20px] flex-wrap">
+                                            {impact?.info?.map((info: any, index: number) => (
+                                                <div key={index}
+                                                     className="flex items-center mr-[20px] my-2 py-[17px] px-[12px] bg-[#F6F6F9] rounded-[4px]">
+                                                    <p className="font-bold text-[28px] text-[#0E0E10] mr-[10px] font-neue">{info.number}</p>
+                                                    <p
+                                                        className=" font-matter text-[17px] uppercase leading-[140%] tracking-[0.17px] text-[#333438]">{info.description}</p>
+                                                </div>))}
                                             <div className="flex ml-[12px] items-center"><Image className="mr-[6px]"
                                                                                                 src="location-pin.svg"
                                                                                                 alt="location"
@@ -82,8 +86,8 @@ const ProjectDetails = ({
                                             </div>
 
                                             <div className="flex mr-[20px] flex-wrap">
-                                                {impact?.info?.map((info: any) => <div
-                                                    className="flex items-center mb-[20px] mr-[20px] py-[17px] px-[12px] bg-[#F6F6F9] rounded-[4px]">
+                                                {impact?.info?.map((info: any, index: number) => <div key={index}
+                                                                                                      className="flex items-center mb-[20px] mr-[20px] py-[17px] px-[12px] bg-[#F6F6F9] rounded-[4px]">
                                                     <p className="font-bold text-[28px] text-[#0E0E10] mr-[10px] font-neue">{info.number}</p>
                                                     <p
                                                         className=" font-matter text-[17px] uppercase leading-[140%] tracking-[0.17px] text-[#333438]">{info.description}</p>
