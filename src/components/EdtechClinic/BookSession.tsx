5fdd7e9d8260557d38577c53c27ae68c9a419f83
Karen Ziboh <karen@cchubnigeria.com>
2024-03-15 17:11:38 +0100

    fix buttons

--- src/components/EdtechClinic/BookSession.tsx
+++ src/components/EdtechClinic/BookSession.tsx
@@ -15,19 +15,19 @@ const BookSession = () => {
         btnText="Book a Session at the Clinic"
         styles="block mb-[64px] lg:b-0 mx-auto lg:mx-0 bg-[#E23F27] px-[36px] py-[23.5px] font-matter font-[500] text-[15px]  md:text-[17px] text-white text-center rounded-[40px]" /> : null}
 
-      {isLinksVisible ? <div><a target="_blank" href="https://bit.ly/cchubedtechclinic"><Button
+      {isLinksVisible ? <><a target="_blank" href="https://bit.ly/cchubedtechclinic"><Button
         btnText="I'm in Nigeria"
         animate={true}
         disabled={false}
         rightIcon="/arrow-right-orange.svg"
-        styles="min-w-max w-full max-w-[300px] block mb-[12px] lg:b-0 mx-auto lg:mx-0 px-[36px] py-[23.5px] font-matter font-[500] text-[15px]  md:text-[17px]] text-[#E23F27] text-center rounded-[40px] bg-white border border-[#E23F27]" /></a>
+        styles="sm:w-[300px] block mb-[12px] lg:b-0 mx-auto lg:mx-0 px-[36px] py-[23.5px] font-matter font-[500] text-[15px] md:text-[17px]] text-[#E23F27] text-center rounded-[40px] bg-white border border-[#E23F27]" /></a>
         <a target="_blank" href="http://bit.ly/ihubedtechclinic"><Button
           btnText="I'm in Kenya"
           animate={true}
           disabled={false}
           rightIcon="/arrow-right-orange.svg"
-          styles="min-w-max w-full max-w-[300px] block mb-[64px] lg:b-0 mx-auto lg:mx-0 px-[36px] py-[23.5px] font-matter font-[500] text-[15px]  md:text-[17px] text-[#E23F27] text-center rounded-[40px] bg-white border border-[#E23F27]" /></a>
-      </div> : null
+          styles="sm:w-[300px] block mb-[64px] lg:b-0 mx-auto lg:mx-0 px-[36px] py-[23.5px] font-matter font-[500] text-[15px] md:text-[17px] text-[#E23F27] text-center rounded-[40px] bg-white border border-[#E23F27]" /></a>
+      </> : null
       }
     </div>
 
